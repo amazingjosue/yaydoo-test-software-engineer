00bb9731664bb31293324d016b57d92d48818a58
Josue Ramirez <jramirez@musicbattles.mx>
2021-08-26 02:05:25 -0500

    Refactor Car insurance update

--- src/coTest.js
+++ src/coTest.js
@@ -9,56 +9,150 @@ class Product {
 class CarInsurance {
   constructor(products = []) {
     this.products = products;
+    this.helpers = new Helpers();
   }
+
   updatePrice() {
-    for (var i = 0; i < this.products.length; i++) {
-      if (this.products[i].name != 'Full Coverage' && this.products[i].name != 'Special Full Coverage') {
-        if (this.products[i].price > 0) {
-          if (this.products[i].name != 'Mega Coverage') {
-            this.products[i].price = this.products[i].price - 1;
-          }
+    try {
+      this.products.map(product => {
+
+        switch (product.name) {
+          case 'Medium Coverage':
+            product = this.helpers.defaultUpdate(product)
+
+            break;
+          case 'Full Coverage':
+            product = this.helpers.fullCoverage(product)
+
+            break;
+          case 'Low Coverage':
+            product = this.helpers.defaultUpdate(product)
+
+            break;
+          case 'Mega Coverage':
+            //Don't change anything
+
+            break;
+          case 'Special Full Coverage':
+            product = this.helpers.specialFullCoverage(product)
+
+            break;
+          case 'Super Sale':
+            product = this.helpers.superSale(product)
+
+            break;
+          default:
+            product = this.helpers.defaultUpdate(product)
+
+            break;
         }
-      } else {
-        if (this.products[i].price < 50) {
-          this.products[i].price = this.products[i].price + 1;
-          if (this.products[i].name == 'Special Full Coverage') {
-            if (this.products[i].sellIn < 11) {
-              if (this.products[i].price < 50) {
-                this.products[i].price = this.products[i].price + 1;
-              }
-            }
-            if (this.products[i].sellIn < 6) {
-              if (this.products[i].price < 50) {
-                this.products[i].price = this.products[i].price + 1;
-              }
-            }
-          }
-        }
-      }
-      if (this.products[i].name != 'Mega Coverage') {
-        this.products[i].sellIn = this.products[i].sellIn - 1;
-      }
-      if (this.products[i].sellIn < 0) {
-        if (this.products[i].name != 'Full Coverage') {
-          if (this.products[i].name != 'Special Full Coverage') {
-            if (this.products[i].price > 0) {
-              if (this.products[i].name != 'Mega Coverage') {
-                this.products[i].price = this.products[i].price - 1;
-              }
-            }
-          } else {
-            this.products[i].price = this.products[i].price - this.products[i].price;
-          }
-        } else {
-          if (this.products[i].price < 50) {
-            this.products[i].price = this.products[i].price + 1;
-          }
-        }
-      }
+
+        return product
+
+      });
+      return this.products;
+
+    } catch (error) {
+      console.error(error)
+    }
+  }
+}
+
+class Helpers {
+  constructor(product) {
+    this.product = product
+  }
+
+  /**
+   * @name
+   * @description
+   * @
+   * @param {Object} product 
+   * @returns {Object} 
+   */
+  defaultUpdate(product) {
+
+    let multiply = 1
+
+    if (product.sellIn <= 0)
+      multiply = 2
+
+    if (product.price > 50)
+      product.price = 50
+
+    if (product.price != 0)
+      product.price = product.price - 1 * multiply
+
+    product.sellIn = product.sellIn - 1
+
+    return product
+  }
+
+  fullCoverage(product) {
+
+    let multiply = 1
+
+    if (product.sellIn <= 0)
+      multiply = 2
+
+    if (product.price < 50)
+      product.price = product.price + (1 * multiply)
+
+    product.sellIn = product.sellIn - 1
+
+    if (product.price > 50)
+      product.price = 50
+
+    return product
+  }
+
+  specialFullCoverage(product) {
+
+    let multiply = 1;
+    let sum = 1;
+
+    if (product.sellIn <= 0) {
+      multiply = 2
+      product.price = product.price = 0
     }
 
-    return this.products;
+    if (product.price != 0 && product.price < 50) {
+      if (product.sellIn < 11) {
+        sum = 2
+        if (product.sellIn < 6) {
+          sum = 3
+        }
+      }
+      product.price = product.price + sum * multiply;
+    }
+
+    product.sellIn = product.sellIn - 1
+
+    if (product.price > 50)
+      product.price = 50
+
+    return product
   }
+
+  superSale(product) {
+
+    let multiply = 1
+
+    if (product.sellIn <= 0)
+      multiply = 2
+
+    if (product.price > 50)
+      product.price = 50
+
+    if (product.price != 0)
+      product.price = product.price - 2 * multiply
+
+    product.sellIn = product.sellIn - 1
+
+    return product
+  }
+
+
 }
 
 module.exports = {
